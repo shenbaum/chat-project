24af3683b32e39397a0b634dd2c80e0131a30a64
shenbaum <81161446+shenbaum@users.noreply.github.com>
2024-04-30 13:42:08 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,13 @@
 import socket
 import threading
+from logingui import *
+
+hostname = socket.gethostname()
+my_ip_address = socket.gethostbyname(hostname)
+
+information = []
+username = 'connected_username'
+can_print = False
 
 class create_socket():
 
@@ -7,29 +15,45 @@ class create_socket():
         self.client_socket = socket.socket()
 
     def connect_to_server(self):
-        self.client_socket.connect(('192.168.1.41', 6969))
+        self.client_socket.connect(('192.168.1.41', 4242))
 
     def get_data(self):
-        return self.client_socket.recv(1024).decode()
+        global can_print
+
+        details = []
+        for i in range(3):
+            details.append(str(self.client_socket.recv(1024).decode()))
+        information.append(details)
+        can_print = True
 
     def send_data(self):
+        global can_print
 
         while(True):
-            data = input('write a message: ')
-            self.client_socket.send(data.encode())
+
+            message = input('enter a message: ')
+            information.append([message, username, str(my_ip_address)])
+
+            for detail in information[-1]:
+                self.client_socket.send(detail.encode())
+            can_print = True
 
     def close_server(self):
         self.client_socket.close()
 
-def socket_procces():
+client_socket = create_socket()
+client_socket.connect_to_server()
 
-    client_socket = create_socket()
-    client_socket.connect_to_server()
+thread = threading.Thread(target = client_socket.send_data)
+thread.start()
 
-    thread = threading.Thread(target = client_socket.send_data)
-    thread.start()
+thread_2 = threading.Thread(target = client_socket.get_data)
+thread_2.start()
 
-    run = True
-    while(run):
-        print('nadav: ' + client_socket.get_data())
-    client_socket.close_server()
+run = True
+while(run):
+    if can_print:
+        for detail in information[-1]:
+            print(detail)
+        can_print = False
+client_socket.client_socket()
