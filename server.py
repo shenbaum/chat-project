bbb6cf9575a96ddb434da1552e3ba91d693a14c7
shenbaum <81161446+shenbaum@users.noreply.github.com>
2024-05-17 00:27:54 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 import socket
 import threading
+from cryptography.fernet import Fernet
 
 HOST = socket.gethostname()
 PORT = 8820
+
+with open('mykey.key', 'rb') as mykey:
+    key = mykey.read()
+
+f = Fernet(key)
 
 class create_socket():
 
@@ -13,6 +19,7 @@ class create_socket():
         self.server_socket.listen()
 
         self.clients = []
+        self.nicknames = []
 
     def broadcast(self, data):
         for client in self.clients:
@@ -23,7 +30,9 @@ class create_socket():
         while(True):
 
             self.client, self.address = self.server_socket.accept()
+            nickname = str(self.client.recv(1024).decode())
             self.clients.append(self.client)
+            self.nicknames.append(nickname)
 
             thread = threading.Thread(target = self.handle, args = (self.client,))
             thread.start()
@@ -31,8 +40,52 @@ class create_socket():
     def handle(self, client):
 
         while(True):
-            data = str(client.recv(1024).decode())
-            self.broadcast(data)
+
+            data = client.recv(1024).decode()
+
+            if data.startswith('/message '):
+                try:
+                    data = data.strip('/message ') # /message yoni hey buddy^nadav^ip
+                    reciever_nickname = data.partition(' ')[0]
+
+                    for nickname in self.nicknames:
+                        if nickname == reciever_nickname:
+
+                            index = self.nicknames.index(reciever_nickname)
+                            data = data.replace(' ', '^', 1) #yoni^hey buddy^nadav^ip
+                            self.clients[index].send(data.encode('utf-8'))
+
+                            client.send(data.encode('utf-8'))
+
+                            break
+                except:
+                    print("Something went wrong")
+
+            elif data.startswith('registered '):
+                with open('enc_info.csv', 'ab') as file:
+                    encrypted_info = data.split()
+                    decrypted_info = f.decrypt(encrypted_info[1])
+
+                    file.write(decrypted_info + b'\n')
+                    file.close()
+
+            elif data.startswith('logged-in '):
+                with open('enc_info.csv', 'rb') as file:
+                    encrypted_info = data.split()
+                    decrypted_info = f.decrypt(encrypted_info[1]) + b'\n'
+
+                    check ='false'
+                    lines = file.readlines()
+
+                    for line in lines:
+                        if decrypted_info == line:
+                            check = 'true'
+                            break
+
+                    client.send(check.encode('utf-8'))
+
+            else:
+                self.broadcast(data)
 
 server_socket = create_socket()
 print('server running...')
